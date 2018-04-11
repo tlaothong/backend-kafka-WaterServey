2c90fba84a91d0a61f9e3277c7705f88dafc9145
zcz12345 <56070057@kmitl.ac.th>
2018-04-11 15:17:43 +0700

    test

--- app.js
+++ app.js
@@ -5,32 +5,23 @@ var bodyParser = require('body-parser')
   app.use( bodyParser.json() );       // to support JSON-encoded bodies
   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
-  })); 
+  }));
 var kafka = require('kafka-node');
 
 var Producer = kafka.Producer,
-    client = new kafka.KafkaClient({kafkaHost:"35.231.191.95:9092,35.231.131.0:9092,35.231.143.31:9092,35.231.255.229:9092",requestTimeout:2000}),
+    client = new kafka.KafkaClient({kafkaHost:"kafka-1:9092,kafka-2:9092,kafka-3:9092,kafka-4:9092",requestTimeout:2000}),
     producer = new Producer(client),
     Consumer = kafka.Consumer,
-    consumer = new Consumer(client,[{topic: 'post-topic'}],{autoCommit:false});
+    consumer = new Consumer(client,[{topic: 'post-topic', partition: 0, offset: -1 }],{autoCommit:false},{fromOffset:true});
 
-app.post('/test',function(req,res){
-        payloads = [
-            { topic: req.body.topic, messages:req.body.message , partition: 0 }
-        ];
-        producer.send(payloads, function (err, data) {
-        if(err)
-            res.json(err)
-        res.json(data);
-        console.log(err)
-        console.log(producer)
-        });
 
 consumer.on('message', function (message) {
-    console.log(message);
+    console.log(data);
+    });
+consumer.on('error', function (err) {
+     console.log('error', err);
     });
 app.listen(5000,function(){
     console.log('Kafka producer running at  5000')
     
-    });
-    })
+});
