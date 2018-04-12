9d88f0ab1eb2813cb1c32f9cf3d95ea5137ab3f9
Tanawat <tanawatgajaseni@gmail.com>
2018-04-12 09:19:37 +0000

    kafka mongodb

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var bodyParser = require('body-parser')
   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
   }));
-var kafka = require('kafka-node');
+var mongoose = require('mongoose'),
+    db = require('./db'),
+    urls = '35.196.18.119'
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://'+urls+':27017/Demo'); 
 
+var kafka = require('kafka-node');
 var Consumer = kafka.Consumer,
     client = new kafka.KafkaClient({kafkaHost:"kafka-1:9092,kafka-2:9092,kafka-3:9092,kafka-4:9092",requestTimeout:2000}),
     consumer = new Consumer(
@@ -34,7 +39,17 @@ var options = {
 var consumerGroup = new ConsumerGroup(options, 'post-topic');
 
 consumerGroup.on('message', function (message) {
-    console.log(message.value);
+    obj = JSON.parse(message.value)
+    if(obj.method =='post') { 
+        var model = mongoose.model(obj.model);
+        var mydata = new model(obj.data);
+	//console.log(model);
+	console.log(mydata);
+        mydata.save(function(err,data){
+        if(err)
+	   console.log(err)
+        });
+    };
     });
 app.listen(5000,function(){
     console.log('Kafka producer running at  5000')
---
+++ db.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+
+var User = new Schema({
+    USERID: {type: String ,unique: true},
+    FIRSTNAME: {type: String},
+    LASTNAME: {type: String},
+    EMAIL: {type: String},
+    PHONE: {type: String},
+    PID : {type: String},
+    PNAME : {type: String},
+    PASSWORD: {type: String},
+    USERNAME: {type: String },
+    STATUS: { type: Boolean },
+    TITLE: {type: String },
+    SSN: {type: String },
+    LEVID: {type: String },
+    TID: {type: String },
+    LEVEL: {type: String },
+    TYPE_NAME: {type: String },
+    CWT: {type: String },
+    CWT_NAME: {type: String },
+});
+module.exports = mongoose.model('User', User);
