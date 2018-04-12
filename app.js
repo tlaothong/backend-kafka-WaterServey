877b6ecd8346457f85f7214199952861e96c1604
zcz12345 <56070057@kmitl.ac.th>
2018-04-12 15:12:37 +0700

    merge

--- app.js
+++ app.js
@@ -8,25 +8,6 @@ var bodyParser = require('body-parser')
   }));
 var kafka = require('kafka-node');
 
-<<<<<<< HEAD
-var Producer = kafka.Producer,
-    client = new kafka.KafkaClient({kafkaHost:"kafka-1:9092,kafka-2:9092,kafka-3:9092,kafka-4:9092",requestTimeout:2000}),
-    producer = new Producer(client),
-    Consumer = kafka.Consumer,
-    consumer = new Consumer(client,[{topic: 'post-topic', partition: 0, offset: -1 }],{autoCommit:false},{fromOffset:true});
-
-
-consumer.on('message', function (message) {
-    console.log(data);
-    });
-consumer.on('error', function (err) {
-     console.log('error', err);
-    });
-app.listen(5000,function(){
-    console.log('Kafka producer running at  5000')
-    
-});
-=======
 var Consumer = kafka.Consumer,
     client = new kafka.KafkaClient({kafkaHost:"kafka-1:9092,kafka-2:9092,kafka-3:9092,kafka-4:9092",requestTimeout:2000}),
     consumer = new Consumer(
@@ -40,7 +21,7 @@ var Consumer = kafka.Consumer,
     );
 var ConsumerGroup = kafka.ConsumerGroup;
 var options = {
-	kafkaHost:"kafka-1:9092,kafka-2:9092,kafka-3:9092,kafka-4:9092",
+	kafkaHost:"kafka-1:9092,kafka-2:9092,35.231.191.95:9092,kafka-4:9092",
 	groupId: 'ExampleTestGroup',
   	autoCommit:false,
 	sessionTimeout: 15000,
@@ -58,4 +39,3 @@ consumerGroup.on('message', function (message) {
 app.listen(5000,function(){
     console.log('Kafka producer running at  5000')
 });
->>>>>>> 4183bf15732486ac0bbec8ac340942f93fdcc01b
